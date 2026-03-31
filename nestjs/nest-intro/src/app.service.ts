b0053d15e888355e4c329b07abd63bd82d7167c1
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2026-03-30 21:45:20 -0300

    feat(controller, service): implement user management functions with array

--- nestjs/nest-intro/src/app.service.ts
+++ nestjs/nest-intro/src/app.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/User.interface';
+
 
 @Injectable()
 export class UsuarioService {
 
+  private users: User[] = [
+    { id: 1, name: 'John Doe', age: 30 },
+    { id: 2, name: 'Jane Smith', age: 25 },
+    { id: 3, name: 'Bob Johnson', age: 40 },
+  ];
 
-  getHelloService(): string {
-    return 'Hello World DESDE SERVICE!';
+
+  getHelloService(): User[] {
+    return this.users;
   }
 
-  postHelloService(): string {
-    return 'Hola POST DESDE SERVICE!';
+  postHelloService(newUser: User): string {
+    console.log("Nuevo usuario:", newUser);
+    
+    this.users.push(newUser);
+    console.log("usuarios", this.users);
+    
+    return 'Usuario agregado correctamente!  🤗';
   }
 
-  deleteHelloService(): string {
-    return 'Adios DELETE DESDE SERVICE!';
+  deleteHelloService(idUser: number): string {
+    const index = this.users.findIndex(user => user.id === idUser);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+      return 'Usuario eliminado correctamente!';
+    }
+    return 'Usuario no encontrado!';
   }
   
-  putHelloService(): string {
-    return 'Hola de nuevo PUT DESDE SERVICE!';
+  putHelloService(updateUser: User): string {
+    const index = this.users.findIndex(user => user.id === updateUser.id);
+    if (index !== -1) {
+      this.users[index] = updateUser;
+      return 'Usuario actualizado correctamente!';
+    }
+    return 'Usuario no encontrado!';
   }
 
 }
